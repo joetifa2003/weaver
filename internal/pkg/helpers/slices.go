3dc58334a66477d714e52699bfbf8031057b2db4
Youssef Ahmed <joetifa2003@gmail.com>
2025-02-22 20:53:58 +0200

    fix: bug in loading freeVars

--- internal/pkg/helpers/slices.go
+++ internal/pkg/helpers/slices.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "iter"
+
 func SliceMap[T any, U any](s []T, f func(T) U) []U {
 	res := make([]U, len(s))
 	for i, v := range s {
@@ -7,4 +9,14 @@ func SliceMap[T any, U any](s []T, f func(T) U) []U {
 	}
 
 	return res
+}
+
+func ReverseIter[T any](s []T) iter.Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		for i := len(s) - 1; i >= 0; i-- {
+			if !yield(i, s[i]) {
+				return
+			}
+		}
+	}
 }

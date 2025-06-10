23e67030c3d7862f489c083ac405567ec4d1271f
Youssef Ahmed <joetifa2003@gmail.com>
2025-06-11 00:41:22 +0300

    feat: rework modules

--- builtin/funcs_modules.go
+++ builtin/funcs_modules.go
@@ -3,37 +3,47 @@ package builtin
 import (
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/joetifa2003/weaver/compiler"
-	"github.com/joetifa2003/weaver/internal/pkg/ds"
 	"github.com/joetifa2003/weaver/ir"
 	"github.com/joetifa2003/weaver/parser"
 	"github.com/joetifa2003/weaver/vm"
 )
 
 func registerBuiltinFuncsModules(builder *vm.RegistryBuilder) {
-	moduleCache := ds.NewConcMap[string, vm.Value]()
+	moduleCache := map[string]vm.Value{}
+	lock := sync.Mutex{}
 
 	builder.RegisterFunc("import", func(v *vm.VM, args vm.NativeFunctionArgs) vm.Value {
+		lock.Lock()
+		defer lock.Unlock()
+
 		pathArg, ok := args.Get(0, vm.ValueTypeString)
 		if !ok {
 			return pathArg
 		}
 
-		builtinModule, ok := v.Executor.Reg.ResolveModule(pathArg.GetString())
+		pathStr := pathArg.GetString()
+
+		if mod, ok := moduleCache[pathStr]; ok {
+			return mod
+		}
+
+		builtinModule, ok := v.Executor.Reg.ResolveModule(pathStr)
 		if ok {
+			moduleCache[pathStr] = builtinModule
 			return builtinModule
 		}
 
-		return initModule(v, pathArg.GetString(), moduleCache)
+		userMod := initModule(v, pathStr)
+		moduleCache[pathStr] = userMod
+		return userMod
 	})
 }
 
-func initModule(v *vm.VM, path string, moduleCache *ds.ConcMap[string, vm.Value]) vm.Value {
+func initModule(v *vm.VM, path string) vm.Value {
 	absPath := filepath.Join(filepath.Dir(v.CurrentFrame().Path), path)
-	if v, ok := moduleCache.Get(absPath); ok {
-		return v
-	}
 
 	srcData, err := os.ReadFile(absPath)
 	if err != nil {
@@ -65,8 +75,6 @@ func initModule(v *vm.VM, path string, moduleCache *ds.ConcMap[string, vm.Value]
 		Path:         absPath,
 		HaltAfter:    true,
 	}, 0)
-
-	moduleCache.Set(absPath, val)
 
 	return val
 }
